51c3be15970f0b937d5acf7e79ff76c24f48c7fa
Stanislav Ievlev <inger@altlinux.org>
2008-09-19 16:24:26 +0400

    - fix runtime translation

--- alterator-sysconfig/ui/sysconfig/index.scm
+++ alterator-sysconfig/ui/sysconfig/index.scm
@@ -24,19 +24,19 @@
   (set-lang (fluid-ref generic-session) (current-language))
 
   ;;wizardface specific hacks
-  (let ((wizard-id (global 'frame:wizard)))
-    (if wizard-id
-      (begin
-	(wizard-id steps-clear)
-	(wizard-id steps (map label+icon (woo-list "/step-list")))
-	(wizard-id current-step 0)
-	(wizard-id action-text 'help (_ "Help" "alterator-wizard"))
-	(wizard-id action-text 'forward (_ "Next" "alterator-wizard")))))
+  (and-let* ((wizard-id (global 'frame:wizard))
+	     (_ (make-translator "alterator-wizard" (session-language))))
+    (wizard-id steps-clear)
+    (wizard-id steps (map label+icon (woo-list "/step-list")))
+    (wizard-id current-step 0)
+    (wizard-id action-text 'help (_ "Help"))
+    (wizard-id action-text 'forward (_ "Next")))
 
   ;;common hacks
-  (label1 text (_ "Select your language:" "alterator-sysconfig"))
-  (label2 text (_ "Please select keyboard switch type:" "alterator-sysconfig"))
-  (keyboard-type enumref "/sysconfig/kbd"))
+  (let ((_ (make-translator "alterator-sysconfig" (session-language))))
+    (label1 text (_ "Select your language:"))
+    (label2 text (_ "Please select keyboard switch type:"))
+    (keyboard-type enumref "/sysconfig/kbd")))
 
 (define (default-language)
   (define-operation get-lang)
@@ -65,25 +65,18 @@
 ;;;;;;;;;;;;
 
 (gridbox
-  columns "30;40;30"
+  columns "100"
+  margin 50
 
-  (spacer)
   (document:id label1 (label text "Select your language:"))
-  (spacer)
 
-  (spacer)
   (document:id langlist (listbox (when selected (update-lang))))
-  (spacer)
 
-  (label colspan 3)
+  (label)
 
-  (spacer)
   (document:id label2 (label text "Please select keyboard switch type:"))
-  (spacer)
 
-  (spacer)
-  (document:id keyboard-type (listbox))
-  (spacer))
+  (document:id keyboard-type (listbox)))
 
 (frame:on-next (thunk (or (write-keyboard) (write-language) 'cancel)))
 
