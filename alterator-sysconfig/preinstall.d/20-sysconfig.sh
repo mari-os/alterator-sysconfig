75edd433e69b64d1b3fe4d5682cd484d41f3499e
Dmitrii Fomchenkov <sirius@altlinux.org>
2023-03-23 15:35:51 +0300

    20-sysconfig.sh: Add the necessary config files
    - Get from "/etc/X11/xinit/Xkbmap" the "layout" and "options"
      parameters needed to initialize the "00-keyboard.conf". This config
      file is required to support X11 Layout.
    - Add initialize of config files required to support VC Keymap. Set the
      "KEYMAP" parameter in the "vconsole.conf". This config file is needed
      VC Keymap.
    - Remove the transfer of "/etc/X11/xinit/Xkbmap" to the destination
      system.

--- alterator-sysconfig/preinstall.d/20-sysconfig.sh
+++ alterator-sysconfig/preinstall.d/20-sysconfig.sh
@@ -17,7 +17,13 @@
 # * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 # ***** END LICENSE BLOCK *****
 
+SYSCONFIG_KEYBOARD="/etc/sysconfig/keyboard"
+SYSCONFIG_CONSOLEFONT="/etc/sysconfig/consolefont"
+VCONSOLE_CONF="/etc/vconsole.conf"
+X11_KEYBOARD_CONF="/etc/X11/xorg.conf.d/00-keyboard.conf"
+
 . install2-init-functions
+. shell-config
 
 mkdir -p "$destdir/etc/sysconfig/"
 
@@ -27,14 +33,41 @@ mkdir -p "$destdir/etc/sysconfig/"
 [ ! -f "/etc/sysconfig/langmap" ] ||
 	install -Dm0644 "/etc/sysconfig/langmap" "$destdir/etc/sysconfig/langmap"
 
-[ ! -f "/etc/sysconfig/consolefont" ] ||
-	install -Dm0644 "/etc/sysconfig/consolefont" "$destdir/etc/sysconfig/consolefont"
+[ ! -f "$SYSCONFIG_CONSOLEFONT" ] ||
+	install -Dm0644 "$SYSCONFIG_CONSOLEFONT" "$destdir/$SYSCONFIG_CONSOLEFONT"
 
-[ ! -f "/etc/sysconfig/keyboard" ] ||
-	install -Dm0644 "/etc/sysconfig/keyboard" "$destdir/etc/sysconfig/keyboard"
+[ ! -f "$SYSCONFIG_KEYBOARD" ] ||
+	install -Dm0644 "$SYSCONFIG_KEYBOARD" "$destdir/$SYSCONFIG_KEYBOARD"
 
-[ ! -f "/etc/X11/xinit/Xkbmap" -o ! -d "$destdir/etc/X11/xinit" ] ||
-	install -Dm0644 "/etc/X11/xinit/Xkbmap" "$destdir/etc/X11/xinit/Xkbmap"
+if [ -f "/etc/X11/xinit/Xkbmap" -o -d "$destdir/etc/X11/xinit" ]; then
+	layout=`shell_config_get "/etc/X11/xinit/Xkbmap" "-layout" "[[:space:]]" | awk '$1=$1' 2>/dev/null`
+	options=`shell_config_get "/etc/X11/xinit/Xkbmap" "-option" "[[:space:]]" | awk '$1=$1' 2>/dev/null`
+
+	install -Dm0644 /dev/null "$destdir/$X11_KEYBOARD_CONF"
+cat > "$destdir/$X11_KEYBOARD_CONF" << EOF
+Section "InputClass"
+Identifier "system-keyboard"
+MatchIsKeyboard "on"
+	Option "XkbLayout" "$layout"
+	Option "XkbOptions" "$options"
+EndSection
+EOF
+fi
 
 [ ! -f "/etc/locale.conf" ] ||
 	install -Dm0644 "/etc/locale.conf" "$destdir/etc/locale.conf"
+
+[ ! -e "$destdir/$VCONSOLE_CONF" ] && install -Dm0644 /dev/null "$destdir/$VCONSOLE_CONF" || :
+if [ -e "$SYSCONFIG_KEYBOARD" -a -e "$SYSCONFIG_CONSOLEFONT" ]; then
+	unset SYSFONT
+	unset SYSFONTACM
+	unset UNIMAP
+	unset KEYTABLE
+
+	[ -e "$SYSCONFIG_KEYBOARD" ] && . "$SYSCONFIG_KEYBOARD" &>/dev/null || :
+	[ -e "$SYSCONFIG_CONSOLEFONT" ] && . "$SYSCONFIG_CONSOLEFONT" &>/dev/null || :
+	[ -n "${SYSFONT-}" ] && shell_config_set "$destdir/$VCONSOLE_CONF" "FONT" $SYSFONT 2>&1 || :
+	[ -n "${SYSFONTACM-}" ] && shell_config_set "$destdir/$VCONSOLE_CONF" "FONT_MAP" $SYSFONTACM 2>&1 || :
+	[ -n "${UNIMAP-}" ] && shell_config_set "$destdir/$VCONSOLE_CONF" "FONT_UNIMAP" $UNIMAP 2>&1 || :
+	[ -n "${KEYTABLE-}" ] && shell_config_set "$destdir/$VCONSOLE_CONF" "KEYMAP" $KEYTABLE 2>&1 || :
+fi
